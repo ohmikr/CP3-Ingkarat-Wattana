b6785dc586d97a68aeb1c0deca9952908642c850
ohmikr <63845255+ohmikr@users.noreply.github.com>
2020-04-18 23:12:50 +0700

    Update Lecture53_Ingkarat_W.py

--- Lecture53_Ingkarat_W.py
+++ Lecture53_Ingkarat_W.py
@@ -1,6 +1,6 @@
-def VatCal(TotalPrice):
+def VatCal():
     TotalPrice = int(input("Product Price:"))
     Result = TotalPrice+(TotalPrice*7/100)
     return Result
 
-print(VatCal(()))
+print(VatCal())
